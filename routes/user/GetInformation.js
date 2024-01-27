9f379fa019f84a645336de7f9a464131011efeb6
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-01-27 14:36:04 +0100

    almost-done

--- routes/user/GetInformation.js
+++ routes/user/GetInformation.js
@@ -1,8 +1,10 @@
 const User = require("../../models/User");
 module.exports = async (req, res) => {
   try {
-    let Users = await User.findOne();
-    res.status(200).json({ status: true, data: Users });
+    let { id } = req.auth;
+    let user = await User.findById(id );
+   ;
+    res.status(200).json({ status: true, data: user });
   } catch (error) {
     if (error) {
       console.log(error);
