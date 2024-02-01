9a2cc8aefb8ecef39d194b28820682e757b2ed95
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-02-01 14:46:29 +0100

    dec qte

--- routes/user/decProductQuantityFromCart.js
+++ routes/user/decProductQuantityFromCart.js
@@ -4,18 +4,25 @@ module.exports = async (req, res) => {
   try {
     let { productId } = req.params;
     let product = await Cart.findOne({ productId });
-    const updatedCart = await Cart.findOneAndUpdate(
-      { productId },
-      {
-        $set: { quantity: product.quantity - 1 },
-      },
-      { new: true }
-    );
+    if (product.quantity > 1) {
+      const updatedCart = await Cart.findOneAndUpdate(
+        { productId },
+        {
+          $set: { quantity: product.quantity - 1 },
+        },
+        { new: true }
+      );
 
-    res.status(200).json({
-      status: true,
-      data: updatedCart,
-    });
+      res.status(200).json({
+        status: true,
+        data: updatedCart,
+      });
+    } else {
+      res.status(400).json({
+        status: false,
+        data: "negative or null values are not available",
+      });
+    }
   } catch (error) {
     if (error) {
       console.log(error);
