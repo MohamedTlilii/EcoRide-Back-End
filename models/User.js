50ea039e3f9dad7afc3a6ba5b73a48fa03ec5242
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-02-02 17:02:50 +0100

    updat

--- models/User.js
+++ models/User.js
@@ -5,18 +5,23 @@ const userSchema = new Schema(
   {
     firstName: {
       type: String,
+      required: [true, "firstName is required field"],
     },
     lastName: {
       type: String,
+      required: [true, "lastName is required field"],
     },
     address: {
       type: String,
+      required: [true, "address is required field"],
     },
     city: {
       type: String,
+      required: [true, "city is required field"],
     },
     number: {
       type: String,
+      required: [true, "number is required field"],
     },
 
     userName: {
