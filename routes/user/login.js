cfc4c0e8d65c9e014742452498cf383a8638dee0
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-02-08 15:16:47 +0100

    updat

--- routes/user/login.js
+++ routes/user/login.js
@@ -28,6 +28,7 @@ module.exports = async (req, res) => {
       data: {
         token,
         isUser: user.isUser,
+        isAdmin: user.isAdmin,
         isBanned: user.isBanned,
       },
     });
