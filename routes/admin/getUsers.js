be24b91893145e17c9376943d6fd0ba94acaa1ca
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-02-08 17:20:53 +0100

    updat

--- routes/admin/getUsers.js
+++ routes/admin/getUsers.js
@@ -2,7 +2,9 @@ const User = require("../../models/User");
 module.exports = async (req, res) => {
   try {
     let { id } = req.auth;
-    let users = await User.find().select("-password -email");
+    let users = await User.find({ _id: { $nin: [id] } }).select(
+      "-password -email"
+    );
     res.status(200).json({ status: true, data: users });
   } catch (error) {
     if (error) {
