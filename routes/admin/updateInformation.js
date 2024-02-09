f80d354194a6759b0188d0443895ed213a581a53
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-02-09 13:41:56 +0100

    updat

--- routes/admin/updateInformation.js
+++ routes/admin/updateInformation.js
@@ -1,16 +1,20 @@
 const Admin = require("../../models/Admin");
-
 module.exports = async (req, res) => {
   try {
-    const { id } = req.auth;
+    let { id } = req.auth;
+    
     await Admin.findByIdAndUpdate(id, {
       $set: {
         ...req.body,
       },
     });
-    res.status(200).json({ status: true, message: "Admin information was updated successfully" });
+    res
+      .status(200)
+      .json({ status: true, message: "Admin Information was updated successfully" });
   } catch (error) {
-    console.error(error);
+    if (error) {
+      console.log(error);
+    }
     res.status(401).json({ status: false, error });
   }
 };
